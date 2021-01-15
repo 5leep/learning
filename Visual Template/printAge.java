5b57e552cbaeb957c32ebd1f30e88668cda7acd6
Sleep <git.sleep@gmail.com>
2021-01-15 13:55:08 +0000

    Add files via upload

---
+++ Visual Template/printAge.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+
+public class printAge {
+
+
+    public static void main (String[] args) {
+        byte age = 20;
+        long largeNumber = 7123987576L;
+        float price = 7.89F;
+        char aLetter = 'E';
+        boolean isForMe = true;
+        Date today = new Date();
+    
+        System.out.println(age);
+        System.out.println(largeNumber);
+        System.out.println(largeNumber);
+        System.out.println(aLetter);
+        System.out.println(isForMe);
+        System.out.println(today);
+    }
+}
